aa0fc029b29c703d8e5b7a107e6a361e0beeaa92
Robin Ward <robin@steelcitysites.net>
2019-03-01 12:23:07 -0600

    removed hometown from html

---
+++ SAPSoftwareIntegration/Migrations/201903011813091_Roi.cs
@@ -0,0 +1,24 @@
+namespace SAPSoftwareIntegration.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Roi : DbMigration
+    {
+        public override void Up()
+        {
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            AlterColumn("dbo.AspNetUsers", "Username", c => c.String());
+            CreateIndex("dbo.AspNetUsers", "UserName", unique: true, name: "UserNameIndex");
+            DropColumn("dbo.AspNetUsers", "Hometown");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "Hometown", c => c.String());
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            AlterColumn("dbo.AspNetUsers", "Username", c => c.String(nullable: false, maxLength: 256));
+            CreateIndex("dbo.AspNetUsers", "UserName", unique: true, name: "UserNameIndex");
+        }
+    }
+}
---
+++ SAPSoftwareIntegration/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace SAPSoftwareIntegration.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SAPSoftwareIntegration.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SAPSoftwareIntegration.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(SAPSoftwareIntegration.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
